ab97b8e290d44c364e92957852e405ddf6991744
lorak <k.zysk@zoho.com>
2015-05-28 18:48:30 +0200

    devide grunt tasks in modules

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,36 +1,14 @@
-// Gruntfile.js
 
-// our wrapper function (required by grunt and its plugins)
-// all configuration goes inside this function
-module.exports = function(grunt) {
-
-  // ===========================================================================
-  // CONFIGURE GRUNT ===========================================================
-  // ===========================================================================
-  grunt.initConfig({
-
-    // configure jshint to validate js files -----------------------------------
-    jshint: {
-      options: {
-        reporter: require('jshint-stylish') // use jshint-stylish to make our errors look and read good
-      },
-
-      // when this task is run, lint the Gruntfile and all js files in src
-      build: ['Gruntfile.js', 'views/js/*.js']
-    }
-
-});
-
-
-  // ===========================================================================
-  // LOAD GRUNT PLUGINS ========================================================
-  // ===========================================================================
-  // we can only load these if they are in our package.json
-  // make sure you have run npm install so our app can find these
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-
+module.exports = function (grunt) {
+  require('load-grunt-tasks')(grunt);
 };
+
+
+
+// module.exports = function(grunt) {
+
+//   require('load-grunt-config')(grunt);
+//   grunt.registerTask('default', function() {
+//     grunt.log.write('Load default...').ok();
+//   });
+// };
