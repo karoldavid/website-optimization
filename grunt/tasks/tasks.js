fb8cf27deec62628b4ace3cb9f424ce84c56f479
lorak <k.zysk@zoho.com>
2015-05-29 20:13:34 +0200

    rm compress

--- grunt/tasks/tasks.js
+++ grunt/tasks/tasks.js
@@ -1,3 +1,3 @@
 module.exports = function (grunt) {
-  grunt.registerTask('default', ['watch', 'jshint', 'htmlhint', 'concat', 'uglify', 'cssmin', 'responsive_images', 'imagemin', 'pagespeed', 'compress']);
+  grunt.registerTask('default', ['watch', 'jshint', 'htmlhint', 'concat', 'uglify', 'cssmin', 'responsive_images', 'imagemin', 'pagespeed']);
 };
