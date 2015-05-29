b27b23938a72ff7f0376e7b5df6bc26595a8ebd8
lorak <k.zysk@zoho.com>
2015-05-29 11:40:36 +0200

    rm comments

--- grunt/options/jshint.js
+++ grunt/options/jshint.js
@@ -1,7 +1,6 @@
 module.exports = {
   options: {
-        reporter: require('jshint-stylish') // use jshint-stylish to make our errors look and read good
+        reporter: require('jshint-stylish')
   },
-      // when this task is run, lint the Gruntfile and all js files in src
         build: ['views/js/*.js']
 };
