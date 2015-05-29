7ee54c4d5ae648ad2f7b57af9b9d60e87d3cc9eb
lorak <k.zysk@zoho.com>
2015-05-29 11:41:04 +0200

    add task pagespeed to grunt

--- grunt/tasks/tasks.js
+++ grunt/tasks/tasks.js
@@ -1,5 +1,5 @@
 module.exports = function (grunt) {
-  grunt.registerTask('default', ['watch', 'jshint', 'htmlhint', 'concat', 'uglify', 'cssmin', 'responsive_images', 'imagemin']);
+  grunt.registerTask('default', ['watch', 'jshint', 'htmlhint', 'concat', 'uglify', 'cssmin', 'responsive_images', 'imagemin', 'pagespeed']);
 
   // grunt.registerTask('dev', ['uglify:dev']);
   // grunt.registerTask('production', ['jshint:production', 'htmlhint:production']);
