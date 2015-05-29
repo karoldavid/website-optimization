fa8784853ceebfe79acc0ccd9fb5ae41e3228a5f
lorak <k.zysk@zoho.com>
2015-05-29 12:17:54 +0200

    change htmlhint options

--- grunt/options/htmlhint.js
+++ grunt/options/htmlhint.js
@@ -1,14 +1,19 @@
 module.exports = {
-  html1: {
-          options: {
-            'tag-pair': true
+  options: {
+            'tag-pair': true,
+            'tagname-lowercase': true,
+            'attr-lowercase': true,
+            'attr-value-double-quotes': true,
+            'doctype-first': true,
+            'spec-char-escape': true,
+            'id-unique': true,
+            'head-script-disabled': true,
+            'style-disabled': false
         },
+  html1: {
         src: ['*.html']
       },
-  html2: {
-          options: {
-            'tag-pair': true
-        },
+  html2: { 
         src: ['views/*.html']
       }
 };
