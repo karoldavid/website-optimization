7d16cd90ee80a289a29334c8149fda9e8078b664
lorak <k.zysk@zoho.com>
2015-05-29 20:12:02 +0200

    rm commenet code

--- grunt/tasks/tasks.js
+++ grunt/tasks/tasks.js
@@ -1,6 +1,3 @@
 module.exports = function (grunt) {
   grunt.registerTask('default', ['watch', 'jshint', 'htmlhint', 'concat', 'uglify', 'cssmin', 'responsive_images', 'imagemin', 'pagespeed', 'compress']);
-
-  // grunt.registerTask('dev', ['uglify:dev']);
-  // grunt.registerTask('production', ['jshint:production', 'htmlhint:production']);
 };
