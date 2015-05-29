71b5e26be6ce9c7cf65c5b523aba8d772ea3b40f
lorak <k.zysk@zoho.com>
2015-05-29 12:19:31 +0200

    add tasks pagespeed and compress to grunt

---
+++ grunt/options/compress.js
@@ -0,0 +1,12 @@
+// gzip assets 1-to-1 for production
+module.exports = {
+  main: {
+    options: {
+      mode: 'gzip'
+    },
+    expand: true,
+    cwd: 'assets/',
+    src: ['**/*'],
+    dest: 'public/'
+  }
+};
---
+++ grunt/options/pagespeed.js
@@ -0,0 +1,22 @@
+module.exports = {
+  options: {
+    nokey: true,
+    url: "https://developers.google.com"
+  },
+  desk: {
+    options: {
+      url: "http://karoldavid.github.io/frontend-nanodegree-mobile-portfolio/",
+      locale: "en_GB",
+      strategy: "desktop",
+      threshold: 91
+    }
+  },
+  mob: {
+    options: {
+      url: ["http://karoldavid.github.io/frontend-nanodegree-mobile-portfolio/"],
+      locale: "en_GB",
+      strategy: "mobile",
+      threshold: 91
+    }
+  }
+};
